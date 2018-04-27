0ec6cc8a80ae640a552390ec6232c70000eb07a2
xnnxvxlxinx <32514087+xnnxvxlxinx@users.noreply.github.com>
2018-04-27 09:17:07 -0500

    8

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -178,22 +178,13 @@ module.exports = [
         "merges": [
             "window.FileSystem"
         ]
-    },
-    {
-        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-        "id": "cordova-sqlite-storage.SQLitePlugin",
-        "pluginId": "cordova-sqlite-storage",
-        "clobbers": [
-            "SQLitePlugin"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-file": "6.0.1",
-    "cordova-sqlite-storage": "2.3.1"
+    "cordova-plugin-file": "6.0.1"
 }
 // BOTTOM OF METADATA
 });
